c88e5b9b3f76d7be021b057f9a10b4f0738f822c
yagishuhei <shuhei.y3166@gmail.com>
2022-08-30 22:47:44 +0900

    [Fix]function

--- app/views/layouts/app.js
+++ app/views/layouts/app.js
@@ -4,14 +4,4 @@ alert(hello);
 function addString(strA){
   let addStr = "Hello" + strA;
   return addStr;
-
 };
-
-$(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
-  });
-});
